b030aeca779b23f2d7616f726a430a81647c33e1
emreo <emreo@comodo.net>
2020-08-06 00:09:21 +0300

    initial graal

---
+++ hello-app-graalvm/src/test/java/com/eo/helloapp/graal/AppTest.java
@@ -0,0 +1,12 @@
+package com.eo.helloapp.graal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
